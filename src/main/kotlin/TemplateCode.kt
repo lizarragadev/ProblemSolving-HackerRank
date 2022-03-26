422ab3c2b24a27d18295e785fb89020f86b0c010
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2022-03-25 23:20:56 -0400

    Add code tempalte

---
+++ src/main/kotlin/TemplateCode.kt
@@ -0,0 +1,18 @@
+import java.io.PrintWriter
+
+/**
+ * Prepare > Problem Solving > Solve Me First
+ *
+ * https://www.hackerrank.com/challenges/solve-me-first/
+ * @author lizarragadev
+ *
+ */
+
+fun main() {
+    _writer.name()
+    _writer.flush()
+}
+
+fun PrintWriter.name() {
+
+}
